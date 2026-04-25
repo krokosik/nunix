7a270c45e6a6ae700f732e874c6f1cce06931754
krokosik <krokosik@pm.me>
2026-04-25 19:39:45 +0200

    fix: home manager dotfiles path mapping

--- hosts/osiris/home.nix
+++ hosts/osiris/home.nix
@@ -6,16 +6,19 @@
 
   # Link dotfiles
   home.file = {
-    ".config/bash".source = ../../dotfiles/bash;
-    ".config/bin".source = ../../dotfiles/bin;
-    ".config/fastfetch".source = ../../dotfiles/fastfetch;
-    ".config/fish".source = ../../dotfiles/fish;
-    ".config/git".source = ../../dotfiles/git;
-    ".config/mise".source = ../../dotfiles/mise;
-    ".config/nvim".source = ../../dotfiles/nvim;
-    ".config/poetry".source = ../../dotfiles/poetry;
-    ".config/ssh".source = ../../dotfiles/ssh;
-    ".config/starship".source = ../../dotfiles/starship;
+    ".bashrc".source = ../../dotfiles/bash/.bashrc;
+    ".bash_profile".source = ../../dotfiles/bash/.bash_profile;
+    ".local/bin".source = ../../dotfiles/bin/.local/bin;
+    ".config/fastfetch".source = ../../dotfiles/fastfetch/.config/fastfetch;
+    ".config/fish".source = ../../dotfiles/fish/.config/fish;
+    ".gitconfig".source = ../../dotfiles/git/.gitconfig;
+    ".gitconfig.lab".source = ../../dotfiles/git/.gitconfig.lab;
+    ".config/mise".source = ../../dotfiles/mise/.config/mise;
+    ".config/nvim".source = ../../dotfiles/nvim/.config/nvim;
+    ".config/pypoetry".source = ../../dotfiles/poetry/.config/pypoetry;
+    # Skip mapping .ssh/id_kws.pub or let it be if it's there
+    ".ssh/config".source = ../../dotfiles/ssh/.ssh/config;
+    ".config/starship.toml".source = ../../dotfiles/starship/.config/starship.toml;
   };
 
   # Let Home Manager install and manage itself.
