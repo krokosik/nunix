d5164b80e1eb9d8f819a6e9e460864ed07234966
krokosik <krokosik@pm.me>
2026-04-26 19:19:57 +0200

    Set user password via nix

--- modules/common/users.nix
+++ modules/common/users.nix
@@ -4,9 +4,10 @@
   ...
 }:
 {
+  sops.secrets.login_password_hash.neededForUsers = true;
+
   users.users.root = {
-    ## hash: mkpasswd -m SHA-512 -s (initial password: rootpassword)
-    initialHashedPassword = "$6$foNFb4DIEmWrZKtq$TZj8iVMQ40/JSO8or9f89bi9j0wkwbwMSvJAMjB1SDo6dwtwa1sbTXka81MUthFTnG75.i.PO0jg8c5b8E1R50";
+    hashedPasswordFile = config.sops.secrets.login_password_hash.path;
   };
 
   users.defaultUserShell = pkgs.fish;
@@ -23,8 +24,7 @@
       "wheel"
     ];
     uid = 1000;
-    # initial password: ${config.username}
-    initialHashedPassword = "$6$rXrqzLpTR6HBpH/K$50Czgh8PFC4ewGoilgHEbeHdA6mxBp.Ch7kHZuY.103wcSP8jvsd5E.hiSy1nJbTXvMVUQXt.T.UpqxckmZzS/";
+    hashedPasswordFile = config.sops.secrets.login_password_hash.path;
     # keys: id_ed25519.pub
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMKb4VxsZXBODsfl98C8eP4ofNQxrDv//KhhAhOLyRd2 krokosik@legion"
