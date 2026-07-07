4679ddc74cfc1ab8716496e35950fc109b1a44d6
krokosik <krokosik@pm.me>
2026-07-07 21:49:18 +0200

    Include msmtp in flake

--- modules/server/default.nix
+++ modules/server/default.nix
@@ -4,7 +4,7 @@
     # inputs.crowdsec.nixosModules.crowdsec
     # inputs.crowdsec.nixosModules.crowdsec-firewall-bouncer
     # ./crowdsec.nix
-    # ./msmtp.nix
+    ./msmtp.nix
     ./neovim.nix
     ./tailscale.nix
   ];
