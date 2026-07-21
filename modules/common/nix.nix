5dee40757a12074f771da520934afdd7d3f904a4
krokosik <krokosik@pm.me>
2026-07-21 20:41:22 +0200

    Add community cache

--- modules/common/nix.nix
+++ modules/common/nix.nix
@@ -21,6 +21,17 @@ in
       "${user}"
     ];
     download-buffer-size = 500000000;
+
+    # add extra cache substituters for binary cache access (e.g. cachix)
+    substituters = [
+      "https://cache.nixos.org/"
+      "https://cache.nix.cachix.org/"
+    ];
+    # add extra cache signing keys for binary cache access (e.g. cachix)
+    trusted-public-keys = [
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
   };
 
   # Enable git
