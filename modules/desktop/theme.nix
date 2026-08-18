ce5de60581b6f9c42042968fdcf77266ee9dbbc8
krokosik <krokosik@pm.me>
2026-08-18 22:11:03 +0200

    Add cursor

--- modules/desktop/theme.nix
+++ modules/desktop/theme.nix
@@ -23,6 +23,12 @@
     base16Scheme = "${pkgs.base16-schemes}/share/themes/tokyo-night-dark.yaml";
     polarity = "dark";
 
+    cursor = {
+      package = pkgs.bibata-cursors;
+      name = "Bibata-Modern-Classic";
+      size = 24;
+    };
+
     fonts = {
       monospace = {
         package = pkgs.nerd-fonts.jetbrains-mono;
