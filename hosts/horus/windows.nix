80d86cf0e0a935dab8a34687ca78b5744978f36e
krokosik <krokosik@pm.me>
2026-08-19 14:12:09 +0200

    Add windows fonts

--- hosts/horus/windows.nix
+++ hosts/horus/windows.nix
@@ -1,4 +1,23 @@
+{ pkgs, ... }:
 {
+  fonts.fontconfig.enable = true;
+
+  systemd.services.windows-fonts = {
+    description = "Install Windows fonts from the mounted Windows drive";
+    wantedBy = [ "multi-user.target" ];
+    after = [ "mnt-c.mount" ];
+    wants = [ "mnt-c.mount" ];
+    serviceConfig = {
+      Type = "oneshot";
+      RemainAfterExit = true;
+    };
+    script = /* bash */ ''
+      install -d -m 0755 /usr/local/share/fonts
+      ln -sfn /mnt/c/Windows/Fonts /usr/local/share/fonts/WindowsFonts
+      ${pkgs.fontconfig}/bin/fc-cache --force /usr/local/share/fonts/WindowsFonts
+    '';
+  };
+
   fileSystems."/mnt/c" = {
     device = "/dev/disk/by-uuid/54E87E4BE87E2AFE";
     fsType = "ntfs3";
