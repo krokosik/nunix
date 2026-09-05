9c3b9c6c2d4ba9261097408bd43e7a32d6cf27e5
krokosik <krokosik@pm.me>
2026-09-05 19:20:18 +0200

    Base for isis (surface) deployment

---
+++ hosts/isis/configuration.nix
@@ -0,0 +1,36 @@
+{
+  config,
+  inputs,
+  ...
+}:
+{
+  imports = [
+    ./disko-config.nix
+    ./windows.nix
+    inputs.nixos-hardware.nixosModules.microsoft-surface-pro-intel
+    ../../modules/boot-limine.nix
+    ../../modules/desktop
+    ../../modules/hibernation.nix
+    # ../../modules/power.nix
+  ];
+
+  # Add the report generated with `run0 nix run github:numtide/nixos-facter --
+  # --output hosts/horus/facter.json` before evaluating or deploying this host.
+  hardware.facter.reportPath = ./facter.json;
+  
+  # hardware.nvidia.branch = "legacy_580";
+
+  role = "desktop";
+
+  hardware.microsoft-surface.kernelVersion = "stable";
+
+  home-manager.users.${config.username} = {
+    imports = [
+      ../../home-manager/base.nix
+      ../../home-manager/desktop
+      ./apps
+    ];
+  };
+
+  system.stateVersion = "26.05";
+}
