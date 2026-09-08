03fd334442929f3cc5fa1228d998477748b2cec1
krokosik <krokosik@pm.me>
2026-09-08 18:55:28 +0200

    Disable broken hyprgrass for now

--- hosts/isis/apps/tablet.nix
+++ hosts/isis/apps/tablet.nix
@@ -14,18 +14,18 @@ in
 {
   home.packages = singleton pkgs.iio-hyprland;
 
-  wayland.windowManager.hyprland = {
-    plugins = singleton hyprlandPlugins.hyprgrass;
+  # wayland.windowManager.hyprland = {
+  #   # plugins = singleton hyprlandPlugins.hyprgrass;
 
-    settings = {
-      # Rotate the built-in panel and touch input using iio-sensor-proxy.
-      exec-once = singleton "${getExe pkgs.iio-hyprland} eDP-1";
+  #   settings = {
+  #     # Rotate the built-in panel and touch input using iio-sensor-proxy.
+  #     exec-once = singleton "${getExe pkgs.iio-hyprland} eDP-1";
 
-      plugin.touch_gestures = {
-        # Upstream recommends higher sensitivity on tablet screens.
-        sensitivity = 4.0;
-        workspace_swipe_fingers = 3;
-      };
-    };
-  };
+  #     plugin.touch_gestures = {
+  #       # Upstream recommends higher sensitivity on tablet screens.
+  #       sensitivity = 4.0;
+  #       workspace_swipe_fingers = 3;
+  #     };
+  #   };
+  # };
 }
