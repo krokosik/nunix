d8c91f3df76a0433c9aaa58f7c89193bef3e8e6d
krokosik <krokosik@pm.me>
2026-09-08 20:32:44 +0200

    Bring back nvidia initrd

--- hosts/horus/configuration.nix
+++ hosts/horus/configuration.nix
@@ -11,16 +11,23 @@
     ../../modules/boot-limine.nix
     ../../modules/desktop
     ../../modules/hibernation.nix
-    # ../../modules/power.nix
   ];
 
   # Add the report generated with `run0 nix run github:numtide/nixos-facter --
   # --output hosts/horus/facter.json` before evaluating or deploying this host.
   hardware.facter.reportPath = ./facter.json;
   
-  # hardware.nvidia.branch = "legacy_580";
-
   role = "desktop";
+
+  boot.initrd = {
+    # we don't use nvidia gpu module, as we let nixos-hardware set the configs
+    kernelModules = [
+      "nvidia"
+      "nvidia_modeset"
+      "nvidia_uvm"
+      "nvidia_drm"
+    ];
+  };
 
   # support building for anubis
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
