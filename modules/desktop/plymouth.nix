d70cb731d12effca00e8bdae0cc415129f2124e2
krokosik <krokosik@pm.me>
2026-09-14 23:38:12 +0200

    Add clear tty service for plymout

--- modules/desktop/plymouth.nix
+++ modules/desktop/plymouth.nix
@@ -33,5 +33,26 @@
     plymouth-poweroff.wantedBy = [ "poweroff.target" ];
     plymouth-halt.wantedBy = [ "halt.target" ];
     plymouth-reboot.wantedBy = [ "reboot.target" ];
+
+    clear-tty-on-shutdown = {
+      description = "Clear TTY to hide text flashes during shutdown handoff";
+      before = [
+        "plymouth-poweroff.service"
+        "plymouth-reboot.service"
+        "plymouth-halt.service"
+      ];
+      wantedBy = [
+        "poweroff.target"
+        "reboot.target"
+        "halt.target"
+      ];
+      serviceConfig = {
+        Type = "oneshot";
+        # \033[2J clears the screen, \033[3J clears the scrollback, \033[H moves cursor to 0,0
+        ExecStart = "${pkgs.coreutils}/bin/printf '\\033[2J\\033[3J\\033[H'";
+        StandardOutput = "tty";
+        TTYPath = "/dev/tty1";
+      };
+    };
   };
 }
