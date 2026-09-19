6d3756d0400973d253d2c247d46e2c5dcbeeee1e
krokosik <krokosik@pm.me>
2026-09-19 17:04:39 +0200

    Mutable codex config

--- home-manager/private/common/slop.nix
+++ home-manager/private/common/slop.nix
@@ -8,13 +8,28 @@
 let
   rtk = pkgs.unstable.rtk;
   skillsPath = "${../../../ai/skills}";
+  codexConfigDir = config.home.sessionVariables.CODEX_HOME;
+  codexConfigPath = "${codexConfigDir}/config.toml";
+  codexConfigTarget = lib.strings.removePrefix config.home.homeDirectory codexConfigPath;
+  codexConfigSource = config.home.file.${codexConfigTarget}.source;
+  codexConfigStateTarget = lib.strings.removePrefix config.home.homeDirectory "${config.xdg.stateHome}/nunix/codex-config.toml";
 in
 {
   home.packages = [ rtk ];
 
-  home.activation.rtkInit = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    ${lib.getExe rtk} init -g --opencode
-    ${lib.getExe rtk} init -g --codex
+  # Codex records project trust in config.toml, so install a writable copy.
+  home.file.${codexConfigTarget}.enable = lib.mkForce false;
+  home.file.${codexConfigStateTarget} = {
+    source = codexConfigSource;
+    onChange = /* bash */ ''
+      run ${lib.getExe' pkgs.coreutils "mkdir"} --parents ${lib.escapeShellArg codexConfigDir}
+      run ${lib.getExe' pkgs.coreutils "install"} --mode=0600 ${lib.escapeShellArg codexConfigSource} ${lib.escapeShellArg codexConfigPath}
+    '';
+  };
+
+  home.activation.rtkInit = lib.hm.dag.entryAfter [ "writeBoundary" ] /* bash */ ''
+    ${lib.getExe rtk} init --global --opencode
+    CODEX_HOME=${lib.escapeShellArg codexConfigDir} ${lib.getExe rtk} init --global --codex
   '';
 
   sops.secrets = {
