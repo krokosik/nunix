3e4eb948a1fcfb703719f0fdb91bd7a7da4759fb
krokosik <krokosik@pm.me>
2026-09-23 19:49:10 +0200

    Add osiris disko tank config

---
+++ hosts/osiris/storage-disko.nix
@@ -0,0 +1,68 @@
+{
+  disko.devices = {
+    disk = {
+      tank1 = {
+        type = "disk";
+        device = "/dev/disk/by-id/ata-WDC_WD40EFZZ-68CPAN0_WD-WX52D56KTHH8";
+        content = {
+          type = "gpt";
+          partitions.zfs = {
+            end = "-8M";
+            content = {
+              type = "zfs";
+              pool = "tank";
+            };
+          };
+        };
+      };
+      tank2 = {
+        type = "disk";
+        device = "/dev/disk/by-id/ata-WDC_WD40EFZZ-68CPAN0_WD-WX52D56AST5R";
+        content = {
+          type = "gpt";
+          partitions.zfs = {
+            end = "-8M";
+            content = {
+              type = "zfs";
+              pool = "tank";
+            };
+          };
+        };
+      };
+    };
+    zpool.tank = {
+      type = "zpool";
+      mode = "mirror";
+      options = {
+        ashift = "12";
+        autoexpand = "on";
+        cachefile = "none";
+        failmode = "continue";
+      };
+      rootFsOptions = {
+        acltype = "posixacl";
+        canmount = "off";
+        compression = "lz4";
+        devices = "off";
+        dnodesize = "auto";
+        mountpoint = "none";
+        normalization = "formD";
+        relatime = "on";
+        xattr = "sa";
+      };
+      datasets = {
+        data = {
+          type = "zfs_fs";
+          options.mountpoint = "none";
+        };
+        reserved = {
+          type = "zfs_fs";
+          options = {
+            mountpoint = "none";
+            refreservation = "100G";
+          };
+        };
+      };
+    };
+  };
+}
