6c9db20177948f4342e842e0ab6647c3b18aa751
krokosik <krokosik@pm.me>
2026-09-25 10:30:55 +0200

    Remove duplicated fallback DNS

--- modules/common/resolved.nix
+++ modules/common/resolved.nix
@@ -22,10 +22,6 @@ in
         DNSSEC = ifStatic "true";
         MulticastDNS = "resolve";
         Domains = ifStatic [ "~." ];
-        FallbackDNS = ifStatic [
-          "1.1.1.1#one.one.one.one"
-          "1.0.0.1#one.one.one.one"
-        ];
         DNSOverTLS = ifStatic "true";
       };
     };
